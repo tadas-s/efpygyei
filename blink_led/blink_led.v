8100253370ba87e7f87f8f7b7487d7421e382f9c
Tadas Sasnauskas <tadas@yoyo.lt>
2020-06-07 15:50:09 +0100

    blink_led: fix verilator linter warning

--- blink_led/blink_led.v
+++ blink_led/blink_led.v
@@ -8,7 +8,7 @@ input CLK; //input 100Mhz clock
 output LED1;
 output LED2;
 
-reg [23:0] clk_div; // 22 bit counter
+reg [23:0] clk_div; // 24 bit counter
 reg LED1_register;
 reg LED2_register;
 wire led_clock; //signal with approx 0.6Hz clock: 100000000/(2^24)
@@ -18,7 +18,7 @@ assign LED2 = ~LED2_register;
 assign led_clock = clk_div[23];
 
 always @ (posedge CLK) begin
-	clk_div <= clk_div + 22'b1;
+	clk_div <= clk_div + 24'b1;
 end
 
 always @ (posedge led_clock) begin
